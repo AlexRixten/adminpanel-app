bd26f52c800226b98c807f08275146cc9aa0a7b7
Neo <91592026+AlexRixten@users.noreply.github.com>
2022-06-18 21:27:16 +0300

    add new sections

--- src/App.js
+++ src/App.js
@@ -6,11 +6,15 @@ import {
 } from "react-router-dom";
 
 import './app.css';
+import Analytics from "./components/pages/analytics/Analytics";
 import Home from "./components/pages/home/Home";
 import NewProduct from "./components/pages/newProduct/NewProduct";
 import NewUser from "./components/pages/newUser/NewUser";
 import Product from "./components/pages/product/Product";
 import ProductList from "./components/pages/productList/ProductList";
+import Reports from "./components/pages/reports/Reports";
+import Sales from "./components/pages/sales/Sales";
+import Transactions from "./components/pages/transactions/Transactions";
 import User from "./components/pages/user/User";
 import UserList from "./components/pages/userList/UserList";
 
@@ -26,13 +30,16 @@ function App() {
           <Sidebar />
           <Routes>
             <Route exact path="/" element={<Home />} />
+            <Route path="/analytics" element={<Analytics />} />
+            <Route path="/sales" element={<Sales />} />
             <Route path="/users" element={<UserList />} />
             <Route path="/users/:userId" element={<User />} />
             <Route path="/newUser" element={<NewUser />} />
             <Route path="/products" element={<ProductList />} />
             <Route path="/product/:productId" element={<Product />} />
             <Route path="/newProduct" element={<NewProduct />} />
-
+            <Route path="/transactions" element={<Transactions />} />
+            <Route path="/reports" element={<Reports />} />
           </Routes>
         </div>
       </div>
---
+++ src/components/pages/transactions/Transactions.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './transactions.css'
+
+export default function Transactions() {
+  return (
+    <div className='transactions'>Transactions</div>
+  )
+}
